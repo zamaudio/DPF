ce94a1eb0fb4f7651378d6feb2c09ba401c4c173
Damien Zammit <damien@zamaudio.com>
2016-06-13 02:58:11 +1000

    Separate WIN64 from WIN32 build and don't try to build non-linux ttl.
    Signed-off-by: Damien Zammit <damien@zamaudio.com>

--- utils/generate-ttl.sh
+++ utils/generate-ttl.sh
@@ -11,16 +11,27 @@ fi
 
 PWD=`dirname $0`
 
-if [ -f $PWD/lv2_ttl_generator.exe ]; then
-  GEN=$PWD/lv2_ttl_generator.exe
+if [ -f $PWD/lv2_ttl_generator32.exe ]; then
+  GEN=$PWD/lv2_ttl_generator32.exe
   EXT=dll
+  OSX=
+  WIN32=1
+  WIN64=
+elif [ -f $PWD/lv2_ttl_generator64.exe ]; then
+  GEN=$PWD/lv2_ttl_generator64.exe
+  EXT=dll
+  OSX=
+  WIN32=
+  WIN64=1
+elif [ -f $PWD/lv2_ttl_generatorosx ]; then
+  GEN=$PWD/lv2_ttl_generatorosx
+  EXT=dylib
+  OSX=1
+  WIN32=
+  WIN64=
 else
   GEN=$PWD/lv2_ttl_generator
-  if [ -d /Library/Audio ]; then
-    EXT=dylib
-  else
-    EXT=so
-  fi
+  EXT=so
 fi
 
 FOLDERS=`find . -type d -name \*.lv2`
@@ -28,6 +39,16 @@ FOLDERS=`find . -type d -name \*.lv2`
 for i in $FOLDERS; do
   cd $i
   FILE=`ls *.$EXT | sort | head -n 1`
-  $GEN ./$FILE
+  if [ "x$WIN64" == "x1" ]; then
+    #wine64 $GEN ./$FILE
+    echo "Not generating ttl for WIN64 yet"
+  elif [ "x$WIN32" == "x1" ]; then
+    #wine $GEN ./$FILE
+    echo "Not generating ttl for WIN32 yet"
+  elif [ "x$OSX" == "x1" ]; then
+    echo "Not generating ttl for OSX yet"
+  else
+    $GEN ./$FILE
+  fi
   cd ..
 done
